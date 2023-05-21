ca819fe32d090a4f1bdb481e9e296f32e3398ada
Iseul Park <park0586@algonquinlive.com>
2023-05-20 21:59:47 -0400

    adding the correct coordinates for the targets

--- main.js
+++ main.js
@@ -3,12 +3,13 @@ const horizontal = document.querySelector('.horizontal');
 const target = document.querySelector('.target');
 const tag = document.querySelector('.tag');
 
+
+// event fuction for showing coordinates when mouse is moving
 document.addEventListener('mousemove', event => {
   const x = event.clientX;
   const y = event.clientY;
-
   // use backtick ` for clientX,Y property
-  console.log(`${x} ${y}`);
+  //console.log(`${x} ${y}`);
 
   vertical.style.left = `${x}px`;
   horizontal.style.top = `${y}px`;
@@ -17,4 +18,14 @@ document.addEventListener('mousemove', event => {
   tag.style.left =`${x}px`;
   tag.style.top =`${y}px`;
   tag.innerHTML = `${x}px, ${y}px`;
+});
+
+// show the coordinates when user clicked the correct spot
+document.addEventListener('click', event => {
+  const x = event.clientX;
+  const y = event.clientY;
+
+  if ((x >= 320 && x <= 350 && y >= 330 && y <= 360) || (x >= 1040 && x <= 1080 && y >= 540 && y <= 580)) {
+    alert('Congratulation! you found the correct spot!');
+  }
 });
